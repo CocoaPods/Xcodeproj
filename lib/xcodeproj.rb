afbbabf609322db2069c9b0a5f603c9c31cf66fc
Fabio Pelosin <fabiopelosin@gmail.com>
2012-07-06 16:51:30 +0200

    Release 0.2.2

--- lib/xcodeproj.rb
+++ lib/xcodeproj.rb
@@ -1,6 +1,6 @@
 module Xcodeproj
-  VERSION = '0.2.1'
-  
+  VERSION = '0.2.2'
+
   autoload :Config,    'xcodeproj/config'
   autoload :Project,   'xcodeproj/project'
   autoload :Workspace, 'xcodeproj/workspace'
