b5381c33eaa351d9b0df64d4da65a2ffcc788808
Fabio Pelosin <fabiopelosin@gmail.com>
2012-10-20 17:30:32 +0200

    [Bin] Extracted option parsing handling from the CocoaPods gem.

--- lib/xcodeproj.rb
+++ lib/xcodeproj.rb
@@ -1,7 +1,18 @@
 module Xcodeproj
   VERSION = '0.3.5'
 
+  require 'colored'
+  class PlainInformative < StandardError
+  end
+
+  class Informative < PlainInformative
+    def message
+      super !~ /\[!\]/ ? "[!] #{super}\n".red : super
+    end
+  end
+
   autoload :Config,    'xcodeproj/config'
+  autoload :Command,   'xcodeproj/command'
   autoload :Constants, 'xcodeproj/constants'
   autoload :Helper,    'xcodeproj/helper'
   autoload :Project,   'xcodeproj/project'
--- xcodeproj.gemspec
+++ xcodeproj.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |s|
   s.require_paths = %w{ ext lib }
 
   s.add_runtime_dependency 'activesupport', '~> 3.2.6'
+  s.add_runtime_dependency 'colored',       '~> 1.2'
 
   ## Make sure you can build the gem on older versions of RubyGems too:
   s.rubygems_version = "1.6.2"
