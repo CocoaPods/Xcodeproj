83d16a1fff958314d776409359747039b8d6711d
Taras Kalapun <t.kalapun@gmail.com>
2013-02-26 14:37:40 +0100

    Added few yaml output formats for show command, to help developing #55

--- lib/xcodeproj/command/show.rb
+++ lib/xcodeproj/command/show.rb
@@ -10,14 +10,40 @@ module Xcodeproj
       for one.}
       end
 
+      def self.options
+        [
+          ["--format [hash|tree_hash|raw]", "YAML output format, optional"],
+        ].concat(super)
+      end
+
       def initialize(argv)
         xcodeproj_path = argv.shift_argument
         @xcodeproj_path = File.expand_path(xcodeproj_path) if xcodeproj_path
+        
+        if argv.option('--format')
+          @output_format = argv.shift_argument
+        end
+
         super unless argv.empty?
       end
 
       def run
         require 'yaml'
+
+        if @output_format
+          case @output_format.to_sym
+          when :hash
+            puts xcodeproj.to_hash.to_yaml
+          when :tree_hash
+            puts xcodeproj.to_tree_hash.to_yaml
+          when :raw
+            puts xcodeproj.to_yaml
+          else
+            raise Informative, "Unknowh format #{@output_format}!"
+          end
+          return
+        end
+
         pretty_print = xcodeproj.pretty_print
         sections = []
         pretty_print.each do |key, value|
