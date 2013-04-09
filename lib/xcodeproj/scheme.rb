dd0616c8c8f709965f9cdb4f3319a6e50ca2ed00
Victor Barros <vbarros@atimi.com>
2013-04-09 16:20:29 -0700

    replace the XML lib from Nokogiri to REXML

--- lib/xcodeproj/scheme.rb
+++ lib/xcodeproj/scheme.rb
@@ -1,10 +1,10 @@
-require 'nokogiri'
+require 'rexml/document'
 
 module Xcodeproj
       
   class XCScheme
     
-    attr :doc, Nokogiri::XML::Document
+    attr :doc, REXML::Document
     
     attr :container, String
     attr :build_target, Xcodeproj::Project::Object::AbstractTarget
@@ -15,146 +15,151 @@ module Xcodeproj
       @build_target = build_target
       @test_target = test_target
       
-      @doc = Nokogiri::XML::Document.new
-      @doc.encoding = 'UTF-8'
+      @doc = REXML::Document.new
+      @doc << REXML::XMLDecl.new(REXML::XMLDecl::DEFAULT_VERSION, REXML::Encoding::UTF_8)
+      @doc.context[:attribute_quote] = :quote
       
-      scheme = @doc << '<Scheme/>'
-      scheme['LastUpgradeVersion'] = build_target.project.root_object.attributes['LastUpgradeCheck']
-      scheme['version'] = '1.3'
+      scheme = @doc.add_element 'Scheme'
+      scheme.attributes['LastUpgradeVersion'] = build_target.project.root_object.attributes['LastUpgradeCheck']
+      scheme.attributes['version'] = '1.3'
       
-      build_action = scheme.add_child('<BuildAction/>').first
-      build_action['parallelizeBuildables'] = 'YES'
-      build_action['buildImplicitDependencies'] = 'YES'
+      build_action = scheme.add_element 'BuildAction'
+      build_action.attributes['parallelizeBuildables'] = 'YES'
+      build_action.attributes['buildImplicitDependencies'] = 'YES'
       
       if (build_target.product_type == 'com.apple.product-type.application') then
-        build_action_entries = build_action.add_child('<BuildActionEntries/>').first
+        build_action_entries = build_action.add_element 'BuildActionEntries'
         
-        build_action_entry = build_action_entries.add_child('<BuildActionEntry/>').first
-        build_action_entry['buildForTesting'] = 'YES'
-        build_action_entry['buildForRunning'] = 'YES'
-        build_action_entry['buildForProfiling'] = 'YES'
-        build_action_entry['buildForArchiving'] = 'YES'
-        build_action_entry['buildForAnalyzing'] = 'YES'
+        build_action_entry = build_action_entries.add_element 'BuildActionEntry'
+        build_action_entry.attributes['buildForTesting'] = 'YES'
+        build_action_entry.attributes['buildForRunning'] = 'YES'
+        build_action_entry.attributes['buildForProfiling'] = 'YES'
+        build_action_entry.attributes['buildForArchiving'] = 'YES'
+        build_action_entry.attributes['buildForAnalyzing'] = 'YES'
       
-        buildable_reference = build_action_entry.add_child('<BuildableReference/>').first
-        buildable_reference['BuildableIdentifier'] = 'primary'
-        buildable_reference['BlueprintIdentifier'] = build_target.uuid
-        buildable_reference['BuildableName'] = "#{build_target.name}.app"
-        buildable_reference['BlueprintName'] = build_target.name
-        buildable_reference['ReferencedContainer'] = "container:#{container}.xcodeproj"
+        buildable_reference = build_action_entry.add_element 'BuildableReference'
+        buildable_reference.attributes['BuildableIdentifier'] = 'primary'
+        buildable_reference.attributes['BlueprintIdentifier'] = build_target.uuid
+        buildable_reference.attributes['BuildableName'] = "#{build_target.name}.app"
+        buildable_reference.attributes['BlueprintName'] = build_target.name
+        buildable_reference.attributes['ReferencedContainer'] = "container:#{container}.xcodeproj"
       end
       
-      test_action = scheme.add_child('<TestAction/>').first
-      test_action['selectedDebuggerIdentifier'] = 'Xcode.DebuggerFoundation.Debugger.LLDB'
-      test_action['selectedLauncherIdentifier'] = 'Xcode.DebuggerFoundation.Launcher.LLDB'
-      test_action['shouldUseLaunchSchemeArgsEnv'] = 'YES'
-      test_action['buildConfiguration'] = 'Debug'
+      test_action = scheme.add_element 'TestAction'
+      test_action.attributes['selectedDebuggerIdentifier'] = 'Xcode.DebuggerFoundation.Debugger.LLDB'
+      test_action.attributes['selectedLauncherIdentifier'] = 'Xcode.DebuggerFoundation.Launcher.LLDB'
+      test_action.attributes['shouldUseLaunchSchemeArgsEnv'] = 'YES'
+      test_action.attributes['buildConfiguration'] = 'Debug'
       
-      testables = test_action.add_child('<Testables/>').first
+      testables = test_action.add_element 'Testables'
       
-      testable_reference = testables.add_child('<TestableReference/>').first
-      testable_reference['skipped'] = 'NO'
+      testable_reference = testables.add_element 'TestableReference'
+      testable_reference.attributes['skipped'] = 'NO'
       
-      buildable_reference = testable_reference.add_child('<BuildableReference/>').first
-      buildable_reference['BuildableIdentifier'] = 'primary'
-      buildable_reference['BlueprintIdentifier'] = test_target.uuid
-      buildable_reference['BuildableName'] = "#{test_target.name}.octest"
-      buildable_reference['BlueprintName'] = test_target.name
-      buildable_reference['ReferencedContainer'] = "container:#{container}.xcodeproj"
+      buildable_reference = testable_reference.add_element 'BuildableReference'
+      buildable_reference.attributes['BuildableIdentifier'] = 'primary'
+      buildable_reference.attributes['BlueprintIdentifier'] = test_target.uuid
+      buildable_reference.attributes['BuildableName'] = "#{test_target.name}.octest"
+      buildable_reference.attributes['BlueprintName'] = test_target.name
+      buildable_reference.attributes['ReferencedContainer'] = "container:#{container}.xcodeproj"
       
       if (build_target.product_type == 'com.apple.product-type.application') then
-        macro_expansion = test_action.add_child('<MacroExpansion/>').first
+        macro_expansion = test_action.add_element 'MacroExpansion'
       
-        buildable_reference = macro_expansion.add_child('<BuildableReference/>').first
-        buildable_reference['BuildableIdentifier'] = 'primary'
-        buildable_reference['BlueprintIdentifier'] = build_target.uuid
-        buildable_reference['BuildableName'] = "#{build_target.name}.app"
-        buildable_reference['BlueprintName'] = build_target.name
-        buildable_reference['ReferencedContainer'] = "container:#{container}.xcodeproj"
+        buildable_reference = macro_expansion.add_element 'BuildableReference'
+        buildable_reference.attributes['BuildableIdentifier'] = 'primary'
+        buildable_reference.attributes['BlueprintIdentifier'] = build_target.uuid
+        buildable_reference.attributes['BuildableName'] = "#{build_target.name}.app"
+        buildable_reference.attributes['BlueprintName'] = build_target.name
+        buildable_reference.attributes['ReferencedContainer'] = "container:#{container}.xcodeproj"
       end
       
-      launch_action = scheme.add_child('<LaunchAction/>').first
-      launch_action['selectedDebuggerIdentifier'] = 'Xcode.DebuggerFoundation.Debugger.LLDB'
-      launch_action['selectedLauncherIdentifier'] = 'Xcode.DebuggerFoundation.Launcher.LLDB'
-      launch_action['launchStyle'] = '0'
-      launch_action['useCustomWorkingDirectory'] = 'NO'
-      launch_action['buildConfiguration'] = 'Debug'
-      launch_action['ignoresPersistentStateOnLaunch'] = 'NO'
-      launch_action['debugDocumentVersioning'] = 'YES'
-      launch_action['allowLocationSimulation'] = 'YES'
+      launch_action = scheme.add_element 'LaunchAction'
+      launch_action.attributes['selectedDebuggerIdentifier'] = 'Xcode.DebuggerFoundation.Debugger.LLDB'
+      launch_action.attributes['selectedLauncherIdentifier'] = 'Xcode.DebuggerFoundation.Launcher.LLDB'
+      launch_action.attributes['launchStyle'] = '0'
+      launch_action.attributes['useCustomWorkingDirectory'] = 'NO'
+      launch_action.attributes['buildConfiguration'] = 'Debug'
+      launch_action.attributes['ignoresPersistentStateOnLaunch'] = 'NO'
+      launch_action.attributes['debugDocumentVersioning'] = 'YES'
+      launch_action.attributes['allowLocationSimulation'] = 'YES'
       
       if (build_target.product_type == 'com.apple.product-type.application') then
-        buildable_product_runnable = launch_action.add_child('<BuildableProductRunnable/>').first
+        buildable_product_runnable = launch_action.add_element 'BuildableProductRunnable'
       
-        buildable_reference = buildable_product_runnable.add_child('<BuildableReference/>').first
-        buildable_reference['BuildableIdentifier'] = 'primary'
-        buildable_reference['BlueprintIdentifier'] = build_target.uuid
-        buildable_reference['BuildableName'] = "#{build_target.name}.app"
-        buildable_reference['BlueprintName'] = build_target.name
-        buildable_reference['ReferencedContainer'] = "container:#{container}.xcodeproj"
+        buildable_reference = buildable_product_runnable.add_element 'BuildableReference'
+        buildable_reference.attributes['BuildableIdentifier'] = 'primary'
+        buildable_reference.attributes['BlueprintIdentifier'] = build_target.uuid
+        buildable_reference.attributes['BuildableName'] = "#{build_target.name}.app"
+        buildable_reference.attributes['BlueprintName'] = build_target.name
+        buildable_reference.attributes['ReferencedContainer'] = "container:#{container}.xcodeproj"
       end
       
-      additional_options = launch_action.add_child('<AdditionalOptions/>').first
+      additional_options = launch_action.add_element 'AdditionalOptions'
       
-      profile_action = scheme.add_child('<ProfileAction/>').first
-      profile_action['shouldUseLaunchSchemeArgsEnv'] = 'YES'
-      profile_action['savedToolIdentifier'] = ''
-      profile_action['useCustomWorkingDirectory'] = 'NO'
-      profile_action['buildConfiguration'] = 'Release'
-      profile_action['debugDocumentVersioning'] = 'YES'
+      profile_action = scheme.add_element 'ProfileAction'
+      profile_action.attributes['shouldUseLaunchSchemeArgsEnv'] = 'YES'
+      profile_action.attributes['savedToolIdentifier'] = ''
+      profile_action.attributes['useCustomWorkingDirectory'] = 'NO'
+      profile_action.attributes['buildConfiguration'] = 'Release'
+      profile_action.attributes['debugDocumentVersioning'] = 'YES'
       
       if (build_target.product_type == 'com.apple.product-type.application') then
-        buildable_product_runnable = profile_action.add_child('<BuildableProductRunnable/>').first
+        buildable_product_runnable = profile_action.add_element 'BuildableProductRunnable'
       
-        buildable_reference = buildable_product_runnable.add_child('<BuildableReference/>').first
-        buildable_reference['BuildableIdentifier'] = 'primary'
-        buildable_reference['BlueprintIdentifier'] = build_target.uuid
-        buildable_reference['BuildableName'] = "#{build_target.name}.app"
-        buildable_reference['BlueprintName'] = build_target.name
-        buildable_reference['ReferencedContainer'] = "container:#{container}.xcodeproj"
+        buildable_reference = buildable_product_runnable.add_element 'BuildableReference'
+        buildable_reference.attributes['BuildableIdentifier'] = 'primary'
+        buildable_reference.attributes['BlueprintIdentifier'] = build_target.uuid
+        buildable_reference.attributes['BuildableName'] = "#{build_target.name}.app"
+        buildable_reference.attributes['BlueprintName'] = build_target.name
+        buildable_reference.attributes['ReferencedContainer'] = "container:#{container}.xcodeproj"
       end
       
-      analyze_action = scheme.add_child('<AnalyzeAction/>').first
-      analyze_action['buildConfiguration'] = 'Debug'
+      analyze_action = scheme.add_element 'AnalyzeAction'
+      analyze_action.attributes['buildConfiguration'] = 'Debug'
       
-      archive_action = scheme.add_child('<ArchiveAction/>').first
-      archive_action['buildConfiguration'] = 'Release'
-      archive_action['revealArchiveInOrganizer'] = 'YES'
+      archive_action = scheme.add_element 'ArchiveAction'
+      archive_action.attributes['buildConfiguration'] = 'Release'
+      archive_action.attributes['revealArchiveInOrganizer'] = 'YES'
     end
     
     def set_build_target_for_running(value)
-      build_action = @doc.root.children.at('BuildAction')
+      build_action = @doc.root.elements['BuildAction']
       
-      if (build_action.children.at('BuildActionEntries') == nil) then
-        build_action_entries = build_action.add_child('<BuildActionEntries/>').first
+      if (build_action.elements['BuildActionEntries'] == nil) then
+        build_action_entries = build_action.add_element 'BuildActionEntries'
       else
-        build_action_entries = build_action.children.at('BuildActionEntries').first
+        build_action_entries = build_action.elements['BuildActionEntries']
       end
       
-      if (build_action_entries.children.at('BuildActionEntry') == nil) then
-        build_action_entry = build_action_entries.add_child('<BuildActionEntry/>').first
-        build_action_entry['buildForTesting'] = 'YES'
-        build_action_entry['buildForProfiling'] = 'NO'
-        build_action_entry['buildForArchiving'] = 'NO'
-        build_action_entry['buildForAnalyzing'] = 'NO'
+      if (build_action_entries.elements['BuildActionEntry'] == nil) then
+        build_action_entry = build_action_entries.add_element 'BuildActionEntry'
+        build_action_entry.attributes['buildForTesting'] = 'YES'
+        build_action_entry.attributes['buildForProfiling'] = 'NO'
+        build_action_entry.attributes['buildForArchiving'] = 'NO'
+        build_action_entry.attributes['buildForAnalyzing'] = 'NO'
       else
         build_action_entry = build_action.children.at('BuildActionEntry').first
       end
       
-      build_action_entry['buildForRunning'] = value
+      build_action_entry.attributes['buildForRunning'] = value
       
-      if (build_action_entry.children.at('BuildableReference') == nil) then
-        buildable_reference = build_action_entry.add_child('<BuildableReference/>').first
-        buildable_reference['BuildableIdentifier'] = 'primary'
-        buildable_reference['BlueprintIdentifier'] = build_target.uuid
-        buildable_reference['BuildableName'] = "#{build_target.name}.octest"
-        buildable_reference['BlueprintName'] = build_target.name
-        buildable_reference['ReferencedContainer'] = "container:#{container}.xcodeproj"
+      if (build_action_entry.elements['BuildableReference'] == nil) then
+        buildable_reference = build_action_entry.add_element 'BuildableReference'
+        buildable_reference.attributes['BuildableIdentifier'] = 'primary'
+        buildable_reference.attributes['BlueprintIdentifier'] = build_target.uuid
+        buildable_reference.attributes['BuildableName'] = "#{build_target.name}.octest"
+        buildable_reference.attributes['BlueprintName'] = build_target.name
+        buildable_reference.attributes['ReferencedContainer'] = "container:#{container}.xcodeproj"
       end
     end
     
     def to_s
-      @doc.to_xml
+      formatter = REXML::Formatters::Pretty.new(2)
+      formatter.compact = true
+      out = ''
+      formatter.write(@doc, out)
+      out
     end
     
     def save_as(project_path)
