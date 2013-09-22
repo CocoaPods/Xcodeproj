ffbb9effaeb9bd5d79b6a225b6cd28beaef60a40
Jason Prado <jason.prado@gmail.com>
2013-09-22 16:38:18 -0700

    Refactor XCScheme API to enable multiple targets

---
+++ spec/fixtures/Sample Project/iOS staticLibrary/iOS_staticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  iOS_staticLibrary.h
+//  iOS staticLibrary
+//
+//  Created by Jason Prado on 9/22/13.
+//  Copyright (c) 2013 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOS_staticLibrary : NSObject
+
+@end
