91ad58d639fa1655377badff27c61d2498da3869
Eloy Durán <eloy.de.enige@gmail.com>
2014-01-26 18:37:29 +0100

    [Config] Store locations with build setting fields.

--- lib/xcodeproj/config/parser.rb
+++ lib/xcodeproj/config/parser.rb
@@ -5,15 +5,18 @@ module Xcodeproj
     attr_accessor :settings
 
     class BuildSetting < Struct.new(:name, :value)
-      class Field < Struct.new(:value, :defined_at)
-        alias_method :to_s, :value
-
+      class Field < Struct.new(:type, :content, :defined_at)
         class Location < Struct.new(:container, :line_number, :character_number)
         end
       end
     end
 
     module Parser
+      def self.create_field(token, container)
+        location = BuildSetting::Field::Location.new(container, token[:line_number], token[:character_number])
+        BuildSetting::Field.new(token[:type], token[:token], location)
+      end
+
       # Container should be Pathname when from a xcconfig file, or a Xcodeproj::Project in case it's defined inside the project.
       def self.parse(input, container = nil)
         config = Config.new
@@ -21,13 +24,19 @@ module Xcodeproj
 
         current_setting = nil
         Lexer.lex_config(input).each do |token|
-          location = BuildSetting::Field::Location.new(container, token[:line_number], token[:character_number])
-          field = BuildSetting::Field.new(token[:token], location)
-
+          next if token[:type] == :comment
           if current_setting.nil? && token[:type] == :setting
-            current_setting = BuildSetting.new(field)
+            current_setting = BuildSetting.new(create_field(token, container))
           elsif current_setting && token[:type] == :value
-            current_setting.value = field
+            value_fields = []
+            Lexer.lex_value(token[:token]).each do |value_token|
+              next if value_token[:type] == :space
+              value_token[:line_number] = token[:line_number]
+              # TODO eh, what's with the off by one?
+              value_token[:character_number] += token[:character_number] - 1
+              value_fields << create_field(value_token, container)
+            end
+            current_setting.value = value_fields
             config.settings << current_setting
             current_setting = nil
           else
--- spec/config/parser_spec.rb
+++ spec/config/parser_spec.rb
@@ -2,25 +2,71 @@ require File.expand_path('../../spec_helper', __FILE__)
 require 'xcodeproj/config/parser'
 
 class Xcodeproj::Config
+  #describe Config do
+    #before do
+      #@top_level    = Config.new('TOP_LEVEL' => '1')
+      #@middle_level = Config.new('MIDDLE_LEVEL', '2')
+      #@bottom_level = Config.new('BOTTOM_LEVEL' => '3')
+
+      #@bottom_level.parent = @middle_level
+      #@middle_level.parent = @top_level
+    #end
+
+    #it "merges build settings from its parent" do
+      #@top_level.to_hash.should == { 'TOP_LEVEL' => '1' }
+      #@middle_level.to_hash.should == { 'TOP_LEVEL' => '1', 'MIDDLE_LEVEL' => '2' }
+      #@bottom_level.to_hash.should == { 'TOP_LEVEL' => '1', 'MIDDLE_LEVEL' => '2', 'BOTTOM_LEVEL' => '3' }
+    #end
+
+    #it "finds where a build setting is defined" do
+      #@top_level.defined_at('MIDDLE_LEVEL').should == []
+      #@middle_level.defined_at('MIDDLE_LEVEL').should == [@middle_level]
+      #@bottom_level.defined_at('MIDDLE_LEVEL').should == [@middle_level]
+
+      #@bottom_level['TOP_LEVEL'] = '4'
+      #@bottom_level.defined_at('TOP_LEVEL').should == [@bottom_level, @top_level]
+      #@middle_level.defined_at('TOP_LEVEL').should == [@top_level]
+      #@top_level.defined_at('TOP_LEVEL').should == [@top_level]
+    #end
+  #end
+
   describe Parser do
     it "generates a BuildSetting for a setting and value assignment" do
-      config = Parser.parse('OTHER_LDFLAGS = -framework Foundation')
+      config = Parser.parse('OTHER_LDFLAGS = $(inherited) -framework Foundation')
       setting = config.settings.first
-      setting.should.be.instance_of BuildSetting
-      setting.name.to_s.should == 'OTHER_LDFLAGS'
-      setting.value.to_s.should == '-framework Foundation'
+      setting.name.content.should == 'OTHER_LDFLAGS'
     end
 
-    it "stores the file location info for each setting field" do
+    it "automatically parses setting values" do
+      config = Parser.parse('OTHER_LDFLAGS = $(inherited) -framework Foundation')
+      fields = config.settings.first.value
+      fields.map { |field| [field.type, field.content] }.should == [
+        [:setting, 'inherited'],
+        [:string, '-framework'],
+        [:string, 'Foundation']
+      ]
+    end
+
+    it "stores the file location info for each field" do
       path = Pathname.new('some/custom.xcconfig')
-      config = Parser.parse('OTHER_LDFLAGS = -framework Foundation', path)
+      config = Parser.parse(%{// comment\nOTHER_LDFLAGS = $(inherited) -framework Foundation}, path)
       setting = config.settings.first
+
       setting.name.defined_at.container.should == path
-      setting.name.defined_at.line_number.should == 1
+      setting.name.defined_at.line_number.should == 2
       setting.name.defined_at.character_number.should == 1
-      setting.value.defined_at.container.should == path
-      setting.value.defined_at.line_number.should == 1
-      setting.value.defined_at.character_number.should == 17
+
+      setting.value[0].defined_at.container.should == path
+      setting.value[0].defined_at.line_number.should == 2
+      setting.value[0].defined_at.character_number.should == 19
+
+      setting.value[1].defined_at.container.should == path
+      setting.value[1].defined_at.line_number.should == 2
+      setting.value[1].defined_at.character_number.should == 30
+
+      setting.value[2].defined_at.container.should == path
+      setting.value[2].defined_at.line_number.should == 2
+      setting.value[2].defined_at.character_number.should == 41
     end
   end
 end
