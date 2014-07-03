8b6bf0ea82d4032c86508832316d2b81c0a13b46
JP Simard <jp@jpsim.com>
2014-07-02 17:11:35 -0700

    test to ensure Xcodeproj can save a project after removing a subproject

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -165,6 +165,40 @@ module ProjectSpecs
         new_instance.should == @project
       end
 
+      it "can save a project after removing a subproject" do
+        project = Xcodeproj::Project.open(fixture_path("Sample Project/Cocoa Application.xcodeproj"))
+
+        # UUID's related to ReferencedProject.xcodeproj (subproject)
+        # FIXME: these UUID's should all be deleted automatically when calling 
+        #        remove_from_project on the subproject PBXFileReference
+        #        See https://github.com/CocoaPods/Xcodeproj/issues/158
+        uuids_to_remove = [
+          "5138059B16499F4C001D82AD",
+          "5138059C16499F4C001D82AD",
+          "E5FBB3451635ED35009E96B0",
+          "E5FBB3461635ED35009E96B0",
+          "E5FBB34B1635ED36009E96B0",
+          "E5FBB34C1635ED36009E96B0",
+          "E5FBB34D1635ED36009E96B0",
+          "E5FBB34E1635ED36009E96B0",
+          "E5FBB34F1635ED36009E96B0",
+          "E5FBB3501635ED36009E96B0"
+        ]
+
+        objects_to_remove = project.objects.select { |o| uuids_to_remove.include?(o.uuid) }
+        
+        objects_to_remove.each do |object|
+          object.remove_from_project
+        end
+
+        project.save(@path)
+
+        new_instance = Xcodeproj::Project.open(@path)
+        new_instance.objects.count.should > 0 # make sure we still have a valid project
+        removed_objects = new_instance.objects.select { |o| uuids_to_remove.include?(o.uuid) }
+        removed_objects.count.should == 0
+      end
+
       it "can open a project and save it without altering any information" do
         project = Xcodeproj::Project.open(fixture_path("Sample Project/Cocoa Application.xcodeproj"))
         plist = Xcodeproj.read_plist(fixture_path("Sample Project/Cocoa Application.xcodeproj/project.pbxproj"))
