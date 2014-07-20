376944e6c382f7fd38a428cf942b308b0c8f72aa
Marius Rackwitz <git@mariusrackwitz.de>
2014-07-20 17:54:01 +0200

    [Spec] Rename new spec file

--- spec/project/project_helper_integration_spec.rb
+++ spec/project/project_helper_integration_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../spec_helper', __FILE__)
+require File.expand_path('../../spec_helper', __FILE__)
 
 module ProjectHelperSpecs
   describe Xcodeproj::Project::ProjectHelper do
