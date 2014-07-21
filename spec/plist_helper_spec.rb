1342f3ccaa69a7252206de64025abfd8f7a8ebe4
Fabio Pelosin <fabiopelosin@gmail.com>
2014-07-21 21:03:22 +0200

    PlistHelper: refine specs

--- spec/plist_helper_spec.rb
+++ spec/plist_helper_spec.rb
@@ -2,58 +2,101 @@
 
 require File.expand_path('../spec_helper', __FILE__)
 
-describe Xcodeproj::PlistHelper do
-  extend SpecHelper::TemporaryDirectory
+module ProjectSpecs
+  describe Xcodeproj::PlistHelper do
 
-  before do
-    @plist = temporary_directory + 'plist'
-  end
+    before do
+      @plist = temporary_directory + 'plist'
+    end
 
-  it "coerces the given path object to a string path" do
-    # @plist is a Pathname
-    Xcodeproj::PlistHelper.write_plist({}, @plist)
-    Xcodeproj::PlistHelper.read_plist(@plist).should == {}
-  end
+    describe "In general" do
+      extend SpecHelper::TemporaryDirectory
 
-  it "raises when the given path can't be coerced into a string path" do
-    lambda { Xcodeproj::PlistHelper.write_plist({}, Object.new) }.should.raise TypeError
-  end
+      it "reads an XML plist file" do
+        dir = "Sample Project/Cocoa Application.xcodeproj/"
+        path = fixture_path(dir + 'project.xml.pbxproj')
+        result = Xcodeproj::PlistHelper.read_plist(path)
+        result.keys.should.include?("archiveVersion")
+      end
 
-  it "raises if the given path doesn't exist" do
-    lambda { Xcodeproj::PlistHelper.read_plist('doesnotexist') }.should.raise ArgumentError
-  end
+      if Xcodeproj::PlistHelper.send(:plutil_available?)
+        it "reads an ASCII plist file" do
+          dir = "Sample Project/Cocoa Application.xcodeproj/"
+          path = fixture_path(dir + 'project.pbxproj')
+          result = Xcodeproj::PlistHelper.read_plist(path)
+          result.keys.should.include?("archiveVersion")
+        end
+      end
 
-  it "coerces the given hash to a Hash" do
-    o = Object.new
-    def o.to_hash; { 'from' => 'object' }; end
-    Xcodeproj::PlistHelper.write_plist(o, @plist)
-    Xcodeproj::PlistHelper.read_plist(@plist).should == { 'from' => 'object' }
-  end
+      it "raises if unable to convert an ASCII plist file" do
+        dir = "Sample Project/Cocoa Application.xcodeproj/"
+        path = fixture_path(dir + 'project.pbxproj')
+        Xcodeproj::PlistHelper.expects(:plutil_available?).returns(false)
 
-  it "raises when given a hash that can't be coerced to a Hash" do
-    lambda { Xcodeproj::PlistHelper.write_plist(Object.new, @plist) }.should.raise TypeError
-  end
+        should.raise RuntimeError do
+          Xcodeproj::PlistHelper.read_plist(path)
+        end.message.should.include?('Unable to convert plist to XML')
+      end
 
-  it "coerces keys to strings" do
-    Xcodeproj::PlistHelper.write_plist({ 1 => '1', :symbol => 'symbol' }, @plist)
-    Xcodeproj::PlistHelper.read_plist(@plist).should == { '1' => '1', 'symbol' => 'symbol' }
-  end
+      it "writes an XML plist file" do
+        hash = { "archiveVersion" => '1.0' }
+        Xcodeproj::PlistHelper.write_plist(hash, @plist)
+        @plist.read.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n  <dict>\n    <key>archiveVersion</key>\n    <string>1.0</string>\n  </dict>\n</plist>\n"
+      end
+    end
 
-  it "allows hashes, strings, booleans, and arrays of hashes and strings as values" do
-    hash = {
-      'hash'   => { 'a hash' => 'in a hash' },
-      'string' => 'string',
-      'true_bool' => true,
-      'false_bool' => false,
-      'array'  => ['string in an array', { 'a hash' => 'in an array' }]
-    }
-    Xcodeproj::PlistHelper.write_plist(hash, @plist)
-    Xcodeproj::PlistHelper.read_plist(@plist).should == hash
-  end
+    #-------------------------------------------------------------------------#
 
-  it "handles unicode characters in paths and strings" do
-    plist = @plist.to_s + 'øµ'
-    Xcodeproj::PlistHelper.write_plist({ 'café' => 'før yoµ' }, plist)
-    Xcodeproj::PlistHelper.read_plist(plist).should == { 'café' => 'før yoµ' }
+    describe "Robustness" do
+      extend SpecHelper::TemporaryDirectory
+
+      it "coerces the given path object to a string path" do
+        # @plist is a Pathname
+        Xcodeproj::PlistHelper.write_plist({}, @plist)
+        Xcodeproj::PlistHelper.read_plist(@plist).should == {}
+      end
+
+      it "raises when the given path can't be coerced into a string path" do
+        lambda { Xcodeproj::PlistHelper.write_plist({}, Object.new) }.should.raise TypeError
+      end
+
+      it "raises if the given path doesn't exist" do
+        lambda { Xcodeproj::PlistHelper.read_plist('doesnotexist') }.should.raise ArgumentError
+      end
+
+      it "coerces the given hash to a Hash" do
+        o = Object.new
+        def o.to_hash; { 'from' => 'object' }; end
+        Xcodeproj::PlistHelper.write_plist(o, @plist)
+        Xcodeproj::PlistHelper.read_plist(@plist).should == { 'from' => 'object' }
+      end
+
+      it "raises when given a hash that can't be coerced to a Hash" do
+        lambda { Xcodeproj::PlistHelper.write_plist(Object.new, @plist) }.should.raise TypeError
+      end
+
+      it "coerces keys to strings" do
+        Xcodeproj::PlistHelper.write_plist({ 1 => '1', :symbol => 'symbol' }, @plist)
+        Xcodeproj::PlistHelper.read_plist(@plist).should == { '1' => '1', 'symbol' => 'symbol' }
+      end
+
+      it "allows hashes, strings, booleans, and arrays of hashes and strings as values" do
+        hash = {
+          'hash'   => { 'a hash' => 'in a hash' },
+          'string' => 'string',
+          'true_bool' => true,
+          'false_bool' => false,
+          'array'  => ['string in an array', { 'a hash' => 'in an array' }]
+        }
+        Xcodeproj::PlistHelper.write_plist(hash, @plist)
+        Xcodeproj::PlistHelper.read_plist(@plist).should == hash
+      end
+
+      it "handles unicode characters in paths and strings" do
+        plist = @plist.to_s + 'øµ'
+        Xcodeproj::PlistHelper.write_plist({ 'café' => 'før yoµ' }, plist)
+        Xcodeproj::PlistHelper.read_plist(plist).should == { 'café' => 'før yoµ' }
+      end
+    end
   end
 end
