1047a699e87290026b2450ac0a184451d1c42c96
Fabio Pelosin <fabiopelosin@gmail.com>
2014-07-27 20:40:08 +0200

    [ObjectDictionary] Complete implementation

--- spec/project/object_dictionary_spec.rb
+++ spec/project/object_dictionary_spec.rb
@@ -5,71 +5,108 @@ module ProjectSpecs
     before do
       attribute = AbstractObjectAttribute.new(:references_by_keys, :project_references, PBXProject)
       attribute.classes = [PBXFileReference, PBXGroup]
+      attribute.classes_by_key = {
+        :project_ref   => PBXFileReference,
+        :product_group => PBXGroup
+      }
       @dictionary = Xcodeproj::Project::ObjectDictionary.new(attribute, @project.root_object)
-      @dictionary['projectRef']   =  @project.new(PBXFileReference)
-      @dictionary['productGroup'] =  @project.new(PBXGroup)
     end
 
-    it "returns the attribute that generated the dictionary" do
-      @dictionary.attribute.name.should == :project_references
+    describe "In general" do
+      it "returns the list of the allowed keys" do
+        @dictionary.allowed_keys.should == [:project_ref, :product_group]
+      end
     end
 
-    it "return the owner of the dictionary" do
-      @dictionary.owner.should == @project.root_object
+    describe "Notification enabled methods" do
+      it "stores an object" do
+        object = @project.new(PBXFileReference)
+        @dictionary[:project_ref] = object
+        @dictionary[:project_ref].should == object
+      end
+
+      it "normalizes the key if needed" do
+        object = @project.new(PBXFileReference)
+        @dictionary['ProjectRef'] = object
+        @dictionary[:project_ref].should == object
+      end
+
+      it "raises if a non allowed key is given" do
+        object = @project.new(PBXFileReference)
+        should.raise do
+          @dictionary[:not_allowed] = object
+        end.message.should.include 'Unsupported key'
+      end
+
+      it "raises if a non allowed ISA for the key is given" do
+        object = @project.new(PBXGroup)
+        should.raise do
+          @dictionary[:project_ref] = object
+        end.message.should.include 'Type checking error'
+      end
+
+      it "informs an object that is has been added to the dictionary" do
+        f = @dictionary[:project_ref] = @project.new(PBXFileReference)
+        f.referrers.should.include?(@project.root_object)
+      end
+
+      it "informs an object that the referenced stopped if its associated key is set to nil" do
+        f = @dictionary[:project_ref] = @project.new(PBXFileReference)
+        f.referrers.count.should == 1
+        f.referrers.should.include?(@project.root_object)
+        @dictionary[:project_ref] = nil
+        f.referrers.count.should == 0
+      end
+
+      it "informs an object that the referenced stopped if its associated key is deleted" do
+        f = @dictionary[:project_ref] = @project.new(PBXFileReference)
+        f.referrers.count.should == 1
+        f.referrers.should.include?(@project.root_object)
+        @dictionary.delete(:project_ref)
+        f.referrers.count.should == 0
+      end
     end
 
-    it "returns the plist representation of the dictionary" do
-      project_ref_uuid = @dictionary['projectRef'].uuid
-      product_group_uuid = @dictionary['productGroup'].uuid
-      @dictionary.to_hash.should == {
-        'projectRef' => project_ref_uuid,
-        'productGroup' => product_group_uuid,
-      }
-    end
+    describe "AbstractObject methods" do
+      before do
+        @dictionary[:project_ref] = @project.new(PBXFileReference)
+        @dictionary[:product_group] = @project.new(PBXGroup)
+      end
 
-    it "returns the to tree hash representation of the dictionary" do
-      @dictionary['projectRef'].name = "A product"
-      @dictionary['projectRef'].path = "A path"
-      @dictionary['productGroup'].name = "Products"
-      @dictionary.to_tree_hash.should == {
-        "projectRef"=>{
-          "displayName"=>"A product",
-          "isa"=>"PBXFileReference",
-          "name"=>"A product",
-          "path"=>"A path",
-          "sourceTree"=>"SOURCE_ROOT",
-          "includeInIndex"=>"1"
-        },
-        "productGroup"=>{
-          "displayName"=>"Products",
-          "isa"=>"PBXGroup",
-          "sourceTree"=>"<group>",
-          "name"=>"Products",
-          "children"=>[]
+      it "returns the plist representation of the dictionary" do
+        project_ref_uuid = @dictionary[:project_ref].uuid
+        product_group_uuid = @dictionary[:product_group].uuid
+        @dictionary.to_hash.should == {
+          'ProjectRef' => project_ref_uuid,
+          'ProductGroup' => product_group_uuid,
         }
-      }
+      end
+
+      it "returns the to tree hash representation of the dictionary" do
+        @dictionary[:project_ref].name = "A project"
+        @dictionary[:project_ref].path = "A path"
+        @dictionary[:product_group].name = "Products"
+        @dictionary.to_tree_hash.should == {
+          "ProjectRef"=>{
+            "displayName"=>"A project",
+            "isa"=>"PBXFileReference",
+            "name"=>"A project",
+            "path"=>"A path",
+            "sourceTree"=>"SOURCE_ROOT",
+            "includeInIndex"=>"1"
+          },
+          "ProductGroup"=>{
+            "displayName"=>"Products",
+            "isa"=>"PBXGroup",
+            "sourceTree"=>"<group>",
+            "name"=>"Products",
+            "children"=>[]
+          }
+        }
+      end
     end
 
-    it "informs an object that is has been added to the dictionary" do
-      f = @dictionary['projectRef']
-      f.referrers.should.include?(@project.root_object)
-    end
 
-    it "informs an object that the referenced stopped if its associated key is deleted" do
-      f = @dictionary['projectRef']
-      f.referrers.count.should == 1
-      f.referrers.should.include?(@project.root_object)
-      @dictionary.delete('projectRef')
-      f.referrers.count.should == 0
-    end
-
-    it "informs an object that the referenced stopped if its associated key is set to nil" do
-      f = @dictionary['projectRef']
-      f.referrers.count.should == 1
-      f.referrers.should.include?(@project.root_object)
-      @dictionary['projectRef'] = nil
-      f.referrers.count.should == 0
-    end
   end
 end
 
