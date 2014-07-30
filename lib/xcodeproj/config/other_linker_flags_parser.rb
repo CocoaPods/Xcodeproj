a0cd92e3e0284829ca29c98b8bceb140b8dc9d91
Fabio Pelosin <fabiopelosin@gmail.com>
2014-07-30 19:37:30 +0200

    [OtherLinkerFlagsParser] Small tweaks and documentation

--- lib/xcodeproj/config/other_linker_flags_parser.rb
+++ lib/xcodeproj/config/other_linker_flags_parser.rb
@@ -6,7 +6,11 @@ module Xcodeproj
     #
     module OtherLinkerFlagsParser
 
+      # @return [Hash{Symbol, Array[String]}] Splits the given
+      #         other linker flags value by type.
       #
+      # @param  [String] flags
+      #         The other linker flags value.
       #
       def self.parse(flags)
         flags
@@ -18,7 +22,7 @@ module Xcodeproj
         }
 
         key = nil
-        tokenize(flags).each do |token|
+        split(flags).each do |token|
           case token
           when '-framework'
             key = :frameworks
@@ -38,7 +42,15 @@ module Xcodeproj
         result
       end
 
-      def self.tokenize(flags)
+      # @return [Array<String>] Split the given other linker
+      #         flags value, taking into account quoting and
+      #         the fact that the `-l` flag might omit the
+      #         space.
+      #
+      # @param  [String] flags
+      #         The other linker flags value.
+      #
+      def self.split(flags)
         result = []
         quotes_accumulator = nil
 
