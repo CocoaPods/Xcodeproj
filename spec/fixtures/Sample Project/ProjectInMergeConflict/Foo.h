eefad42e6cb71d2761e88e4072ef82753ba2aa37
Marius Rackwitz <git@mariusrackwitz.de>
2014-09-26 21:44:25 +0200

    [Spec][Fixture] Add ProjectInMergeConflict

---
+++ spec/fixtures/Sample Project/ProjectInMergeConflict/Foo.h
@@ -0,0 +1,13 @@
+//
+//  Foo.h
+//  ProjectInMergeConflict
+//
+//  Created by Marius Rackwitz on 26/09/14.
+//
+//
+
+#ifndef ProjectInMergeConflict_Foo_h
+#define ProjectInMergeConflict_Foo_h
+
+
+#endif
