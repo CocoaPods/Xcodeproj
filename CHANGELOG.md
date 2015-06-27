700e41b26f170d31af2599c50cea5666923a6363
Samuel E. Giddins <segiddins@segiddins.me>
2015-06-26 18:21:05 -0700

    Release 0.25.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Xcodeproj Changelog
 
-## Master
+## 0.25.0
 
 ##### Enhancements
 
--- lib/xcodeproj/gem_version.rb
+++ lib/xcodeproj/gem_version.rb
@@ -1,5 +1,5 @@
 module Xcodeproj
   # The version of the xcodeproj gem.
   #
-  VERSION = '0.24.2' unless defined? Xcodeproj::VERSION
+  VERSION = '0.25.0' unless defined? Xcodeproj::VERSION
 end
