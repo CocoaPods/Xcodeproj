9c13e27b3124c58908ee532bcde7933d1e46e8dd
Olivier Halligon <olivier@halligon.net>
2015-07-28 01:08:25 +0200

    [XCScheme] Added Spec for loading an existing `.xcscheme` file

--- spec/scheme_spec.rb
+++ spec/scheme_spec.rb
@@ -13,6 +13,90 @@ module ProjectSpecs
 
     #-------------------------------------------------------------------------#
 
+    describe 'Load an existing scheme from file' do
+      before do
+        scheme_path = fixture_path('SharedSchemes', 'SharedSchemes.xcodeproj/xcshareddata/xcschemes/SharedSchemes.xcscheme')
+        @scheme = Xcodeproj::XCScheme.new(scheme_path)
+      end
+
+      it 'Properly map the scheme\'s BuildAction' do
+        @scheme.build_action.parallelize_buildables?.should == true
+        @scheme.build_action.build_implicit_dependencies?.should == true
+        @scheme.build_action.entries.count.should == 1
+
+        entry = @scheme.build_action.entries[0]
+        entry.class.should == Xcodeproj::XCScheme::BuildAction::Entry
+        entry.build_for_testing?.should == true
+        entry.build_for_running?.should == true
+        entry.build_for_profiling?.should == true
+        entry.build_for_archiving?.should == true
+        entry.build_for_analyzing?.should == true
+
+        entry.buildable_references.count.should == 1
+        ref = entry.buildable_references[0]
+        ref.target_name.should == 'SharedSchemes'
+        ref.target_uuid.should == '632143E8175736EE0038D40D'
+        ref.buildable_name.should == 'SharedSchemes.app'
+        ref.target_referenced_container.should == 'container:SharedSchemes.xcodeproj'
+      end
+
+      it 'Properly map the scheme\'s TestAction' do
+        @scheme.test_action.should_use_launch_scheme_args_env?.should == true
+        @scheme.test_action.build_configuration.should == 'Debug'
+
+        @scheme.test_action.testables.count.should == 0
+        @scheme.test_action.macro_expansions.count.should == 1
+
+        macro = @scheme.test_action.macro_expansions[0]
+        macro.class.should == Xcodeproj::XCScheme::MacroExpansion
+
+        ref = macro.buildable_reference
+        ref.target_name.should == 'SharedSchemes'
+        ref.target_uuid.should == '632143E8175736EE0038D40D'
+        ref.buildable_name.should == 'SharedSchemes.app'
+        ref.target_referenced_container.should == 'container:SharedSchemes.xcodeproj'
+      end
+
+      it 'Properly map the scheme\'s LaunchAction' do
+        @scheme.launch_action.allow_location_simulation?.should == true
+        @scheme.launch_action.build_configuration.should == 'Debug'
+
+        bpr = @scheme.launch_action.buildable_product_runnable
+        bpr.class.should == Xcodeproj::XCScheme::BuildableProductRunnable
+
+        ref = bpr.buildable_reference
+        ref.target_name.should == 'SharedSchemes'
+        ref.target_uuid.should == '632143E8175736EE0038D40D'
+        ref.buildable_name.should == 'SharedSchemes.app'
+        ref.target_referenced_container.should == 'container:SharedSchemes.xcodeproj'
+      end
+
+      it 'Properly map the scheme\'s ProfileAction' do
+        @scheme.profile_action.should_use_launch_scheme_args_env?.should == true
+        @scheme.profile_action.build_configuration.should == 'Release'
+
+        bpr = @scheme.launch_action.buildable_product_runnable
+        bpr.class.should == Xcodeproj::XCScheme::BuildableProductRunnable
+
+        ref = bpr.buildable_reference
+        ref.target_name.should == 'SharedSchemes'
+        ref.target_uuid.should == '632143E8175736EE0038D40D'
+        ref.buildable_name.should == 'SharedSchemes.app'
+        ref.target_referenced_container.should == 'container:SharedSchemes.xcodeproj'
+      end
+
+      it 'Properly map the scheme\'s AnalyzeAction' do
+        @scheme.analyze_action.build_configuration.should == 'Debug'
+      end
+
+      it 'Properly map the scheme\'s ArchiveAction' do
+        @scheme.archive_action.build_configuration.should == 'Release'
+        @scheme.archive_action.reveal_archive_in_organizer?.should == true
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
     describe 'Serialization' do
       before do
         app = @project.new_target(:application, 'iOS application', :osx)
