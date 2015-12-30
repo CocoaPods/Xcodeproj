3fc7e22fe014ff7f2f4554e29f9c41310c5ef6dd
Samuel Giddins <segiddins@segiddins.me>
2015-12-30 17:36:47 -0500

    Release 1.0.0.beta.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Xcodeproj Changelog
 
-## Master
+## 1.0.0.beta.1 (2015-12-30)
 
 ##### Enhancements
 
--- lib/xcodeproj/gem_version.rb
+++ lib/xcodeproj/gem_version.rb
@@ -1,5 +1,5 @@
 module Xcodeproj
   # The version of the xcodeproj gem.
   #
-  VERSION = '0.28.2' unless defined? Xcodeproj::VERSION
+  VERSION = '1.0.0.beta.1'.freeze unless defined? Xcodeproj::VERSION
 end
