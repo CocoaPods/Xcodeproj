04bdbae220fc59b1df2e832ffc2cc9810f74a25d
Danielle Tomlinson <dan@tomlinson.io>
2016-09-24 15:21:43 +0200

    [FFI] Make Chdir Chdir again.
    The world is sadness.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,9 @@
 
 ##### Bug Fixes
 
-* None.  
+* Cover more cases of the Dir.chdir breakages.  
+  [Danielle Tomlinson](https://github.com/dantoml)
+  [#]()
 
 
 ## 1.3.1 (2016-09-10)
--- lib/xcodeproj/plist/ffi.rb
+++ lib/xcodeproj/plist/ffi.rb
@@ -1,35 +1,6 @@
+
 module Xcodeproj
   module Plist
-    module Extensions
-      # Since Xcode 8 beta 4, calling `PBXProject.projectWithFile` breaks subsequent calls to
-      # `chdir`. While sounding ridiculous, this is unfortunately true and debugging it from the
-      # userland side showed no difference at all to successful calls to `chdir`, but the working
-      # directory is simply not changed in the end. This workaround is even more absurd, monkey
-      # patching all calls to `chdir` to use `__pthread_chdir` which appears to work.
-      module Dir
-        def self.chdir(path)
-          old_dir = Dir.getwd
-          res = actually_chdir(path)
-
-          if block_given?
-            begin
-              return yield
-            ensure
-              actually_chdir(old_dir)
-            end
-          end
-
-          res
-        end
-
-        def self.actually_chdir(path)
-          libc = Fiddle.dlopen '/usr/lib/libc.dylib'
-          f = Fiddle::Function.new(libc['__pthread_chdir'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
-          f.call(path.to_s)
-        end
-      end
-    end
-
     # Provides support for loading and serializing property list files via
     # Fiddle and CoreFoundation / Xcode.
     #
@@ -171,7 +142,12 @@ module Xcodeproj
         end
 
         def monkey_patch_chdir
-          Dir.send(:include, Extensions::Dir)
+          require 'xcodeproj/plist/ffi/chdir_override'
+          Dir.class_eval do
+            class << Dir
+              alias_method :chdir, :cp_chdir
+            end
+          end
         end
       end
     end
