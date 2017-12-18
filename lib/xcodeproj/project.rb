e9a9eb470ac7d93a96c47acaaa60cc49a7aea108
Yadir Hernandez Batista <yadirhb@gmail.com>
2017-12-18 17:38:23 -0300

    Fixing the issue #531
    Originally the code was assuming that every target had the method "test_target_type" but looking into the targets definition on file (xcodeproj/project/object/native_target.rb) I've notice that there are three target types:
    1-PBXNativeTarget
    2-PBXAggregateTarget
    3-PBXLegacyTarget 
    The only target type which defines the method "test_target_type" is PBXNativeTarget, that is why the change proposal adds the type verification check before actually call the referred method.

--- lib/xcodeproj/project.rb
+++ lib/xcodeproj/project.rb
@@ -809,7 +809,7 @@ module Xcodeproj
       targets.each do |target|
         scheme = XCScheme.new
         scheme.add_build_target(target)
-        scheme.add_test_target(target) if target.test_target_type?
+        scheme.add_test_target(target) if target.instance_of?(Object::PBXNativeTarget) && target.test_target_type?
         yield scheme, target if block_given?
         scheme.save_as(path, target.name, false)
         xcschememanagement['SchemeUserState']["#{target.name}.xcscheme"] = {}
