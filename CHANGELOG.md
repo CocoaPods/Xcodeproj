cc06d685d81ac3b1e85c217f045e3c0665d0cd39
Dimitris Koutsogiorgas <dimitris@squareup.com>
2019-03-27 15:01:03 -0700

    Release 1.8.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Xcodeproj Changelog
 
-## Master
+## 1.8.2 (2019-03-27)
 
 ##### Enhancements
 
--- lib/xcodeproj/gem_version.rb
+++ lib/xcodeproj/gem_version.rb
@@ -1,5 +1,5 @@
 module Xcodeproj
   # The version of the xcodeproj gem.
   #
-  VERSION = '1.8.1'.freeze unless defined? Xcodeproj::VERSION
+  VERSION = '1.8.2'.freeze unless defined? Xcodeproj::VERSION
 end
