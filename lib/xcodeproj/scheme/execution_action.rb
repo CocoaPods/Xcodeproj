adf5c92ce71ad435cdf838dfee80185ef058e135
Thiago Cruz <thiago@squareup.com>
2020-10-14 23:24:47 -0700

    Change execute_action validation strategy

--- lib/xcodeproj/scheme/execution_action.rb
+++ lib/xcodeproj/scheme/execution_action.rb
@@ -1,18 +1,18 @@
 module Xcodeproj
   class XCScheme
     class ExecutionAction < XMLElementWrapper
-      def initialize(action_type, node = nil)
-        action_type_string_value = Constants::EXECUTION_ACTION_TYPE[action_type]
-        raise "[Xcodeproj] Invalid ActionType: got `#{type}`" \
-              " available `#{Constants::EXECUTION_ACTION_TYPE.keys}`" if action_type_string_value.nil?
-
-        create_xml_element_with_fallback(node, 'ExecutionAction') do
-          @xml_element.attributes['ActionType'] = action_type_string_value
-        end
+      def initialize(node = nil)
+        create_xml_element_with_fallback(node, 'ExecutionAction')
       end
 
-      def action_type
+      def action_type?
         @xml_element.attributes['ActionType']
+      end
+
+      def action_type=(value)
+        return unless validate_action_type(value)
+
+        @xml_element.attributes['ActionType'] = value
       end
 
       def action_context?
@@ -20,20 +20,41 @@ module Xcodeproj
       end
 
       def action_context=(ctx)
-        valid_context = case action_type
-                        when Constants::EXECUTION_ACTION_TYPE[:shell_script_action]
-                          ctx.is_a?(ShellScriptActionContext)
-                        when Constants::EXECUTION_ACTION_TYPE[:send_email_action]
-                          ctx.is_a?(SendEmailActionContext)
-                        else
-                          false
-                        end
-
-        raise "[Xcodeproj] Invalid ActionContext `#{ctx.class}`" \
-        " for current ActionType #{action_type}, " unless valid_context
+        return unless validate_action_context(ctx)
 
         @xml_element.delete_element('ActionContext')
         @xml_element.add_element(ctx.xml_element)
+      end
+
+      #-------------------------------------------------------------------------#
+
+      private
+
+      # @!group Private helpers
+
+      def validate_action_type(type)
+        return true unless @action_context
+
+        if @action_context.is_a?(ShellScriptActionContext)
+          type == Constants::EXECUTION_ACTION_TYPE[:shell_script_action]
+        elsif @action_context.is_a?(SendEmailActionContext)
+          type == Constants::EXECUTION_ACTION_TYPE[:send_email_action]
+        else
+          raise "[Xcodeproj] Invalid ActionType `#{type}` for ActionContext `#{@action_context.class}`"
+        end
+      end
+
+      def validate_action_context(ctx)
+        return true unless @action_type
+
+        case @action_type
+        when Constants::EXECUTION_ACTION_TYPE[:shell_script_action]
+          ctx.is_a?(ShellScriptActionContext)
+        when Constants::EXECUTION_ACTION_TYPE[:send_email_action]
+          ctx.is_a?(SendEmailActionContext)
+        else
+          raise "[Xcodeproj] Invalid ActionContext `#{ctx.class}` for ActionType `#{@action_type}`"
+        end
       end
     end
   end
