a749a0e52ddfb025a538d33def9aef02104c77c4
Thiago Cruz <thiago@squareup.com>
2020-10-15 11:58:58 -0700

    Add pre and post actions to other scheme actions

--- lib/xcodeproj/scheme/abstract_scheme_action.rb
+++ lib/xcodeproj/scheme/abstract_scheme_action.rb
@@ -23,6 +23,54 @@ module Xcodeproj
       def build_configuration=(config_name)
         @xml_element.attributes['buildConfiguration'] = config_name
       end
+
+      def pre_actions
+        pre_actions = @xml_element.elements['PreActions']
+        return nil unless pre_actions
+        pre_actions.get_elements('ExecutionAction').map do |entry_node|
+          ExecutionAction.new(entry_node)
+        end
+      end
+
+      def pre_actions=(pre_actions)
+        @xml_element.delete_element('PreActions')
+        unless pre_actions.empty?
+          pre_actions_element = @xml_element.add_element('PreActions')
+          pre_actions.each do |entry_node|
+            pre_actions_element.add_element(entry_node.xml_element)
+          end
+        end
+        pre_actions
+      end
+
+      def add_pre_action(pre_action)
+        pre_actions = @xml_element.elements['PreActions'] || @xml_element.add_element('PreActions')
+        pre_actions.add_element(pre_action.xml_element)
+      end
+
+      def post_actions
+        post_actions = @xml_element.elements['PostActions']
+        return nil unless post_actions
+        post_actions.get_elements('ExecutionAction').map do |entry_node|
+          ExecutionAction.new(entry_node)
+        end
+      end
+
+      def post_actions=(post_actions)
+        @xml_element.delete_element('PostActions')
+        unless post_actions.empty?
+          post_actions_element = @xml_element.add_element('PostActions')
+          post_actions.each do |entry_node|
+            post_actions_element.add_element(entry_node.xml_element)
+          end
+        end
+        post_actions
+      end
+
+      def add_post_action(post_action)
+        post_actions = @xml_element.elements['PostActions'] || @xml_element.add_element('PostActions')
+        post_actions.add_element(post_action.xml_element)
+      end
     end
   end
 end
