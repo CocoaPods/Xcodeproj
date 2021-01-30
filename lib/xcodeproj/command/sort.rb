1871efa6dd34a7a31779fa0c13f15e9e728caf27
Connor Ricks <connor.ricks@willowtreeapps.com>
2021-01-30 14:46:37 -0500

    Adds the ability to specify sorting options for groups.

--- lib/xcodeproj/command/sort.rb
+++ lib/xcodeproj/command/sort.rb
@@ -9,22 +9,43 @@ module Xcodeproj
 
       self.summary = 'Sorts the given project.'
 
+      def self.options
+        [
+          ['--group-option=[above|below]', 'The position of the groups when sorting. If no option is specified, sorting will interleave groups and files.'],
+        ].concat(super)
+      end
+      
       self.arguments = [
         CLAide::Argument.new('PROJECT', false),
       ]
 
       def initialize(argv)
         self.xcodeproj_path = argv.shift_argument
+        @group_option = argv.option('group-option')
+        @group_option &&= @group_option.to_sym
         super
       end
-
-      def validate!
+      
+      def validate
         super
+        unless [nil, :above, :below].include?(@group_option)
+          help! "Unknown format `#{@group_option}`"
+        end
         open_project!
       end
 
       def run
-        xcodeproj.sort
+          
+        if @group_option
+            if [:above, :below].include? @group_option
+                xcodeproj.sort(groups_position: @group_option)
+            else
+                help! "Unknown format `#{@group_option}`"
+            end
+        else
+            xcodeproj.sort
+        end
+        
         xcodeproj.save
         puts "The `#{File.basename(xcodeproj_path)}` project was sorted"
       end
